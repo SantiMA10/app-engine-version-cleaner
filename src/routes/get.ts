8efdbc4a1b6ffc3bb65fc4c1651fb100b06fef50
Santiago Martín Agra <santiagomartinagra@gmail.com>
2021-04-18 17:31:16 +0200

    feat: parse Google Cloud Events

--- src/routes/get.ts
+++ src/routes/get.ts
@@ -1,10 +1,20 @@
 import { Request, ResponseToolkit, ServerRoute } from '@hapi/hapi';
+import { HTTP } from 'cloudevents';
 import { StatusCodes } from 'http-status-codes';
 
 export const routes = (): ServerRoute => ({
 	method: 'GET',
 	path: '/',
-	handler: (_: Request, h: ResponseToolkit) => {
-		return h.response({ ok: true }).code(StatusCodes.OK);
+	handler: (request: Request, h: ResponseToolkit) => {
+		const event = { body: request.payload, headers: request.headers };
+
+		if (!HTTP.isEvent(event)) {
+			console.log({ event });
+			return h.response().code(StatusCodes.BAD_REQUEST);
+		}
+
+		const cloudEvent = HTTP.toEvent(event);
+		console.log({ cloudEvent });
+		return h.response({ ok: true, cloudEvent }).code(StatusCodes.OK);
 	},
 });
--- tests/routes/get.spec.ts
+++ tests/routes/get.spec.ts
@@ -1,13 +1,38 @@
+import { Headers } from '@hapi/shot';
+import { CloudEvent, HTTP } from 'cloudevents';
 import { StatusCodes } from 'http-status-codes';
 
 import { initServer } from '../../src/server';
 
 describe('GET /', () => {
-	it('returns a 200 OK HTTP status code', async () => {
+	it('returns a 200 OK HTTP if the request contains an event', async () => {
+		const subject = await initServer();
+		const ce = new CloudEvent({
+			source: '/source',
+			type: 'type',
+			datacontenttype: 'text/plain',
+			dataschema: 'https://d.schema.com/my.json',
+			subject: 'cha.json',
+			data: 'my-data',
+			extension1: 'some extension data',
+		});
+		const message = HTTP.binary(ce);
+
+		const { statusCode } = await subject.inject({
+			method: 'GET',
+			url: '/',
+			headers: message.headers as Headers,
+			payload: message.body as Record<string, unknown>,
+		});
+
+		expect(statusCode).toBe(StatusCodes.OK);
+	});
+
+	it('returns a 400 BAD REQUEST HTTP if the request does not contain an event', async () => {
 		const subject = await initServer();
 
 		const { statusCode } = await subject.inject({ method: 'GET', url: '/' });
 
-		expect(statusCode).toBe(StatusCodes.OK);
+		expect(statusCode).toBe(StatusCodes.BAD_REQUEST);
 	});
 });
